d0306884494267d5301a508e61a78b6907136bcc
NimsaraThenuka <nimsararanasinghe957@gmail.com>
2025-04-26 15:49:22 +0530

    feat:update plan
    Implement create, update, delete, functionalities for Learning Plan(frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,10 @@ import LearningPlan from "./Pages/LearningPlan2";
 import LearningPlanCreator from "./Components/LearningPlanCreator2";
 // import SearchResults from "./Components/SearchResults";
 import SearchResults from "./Components/SearchResults ";
+import LearningPlans from "./Pages/LearningPlans";
+import LearningPlanDetail from "./Pages/LearningPlanDetail";
+import CreateLearningPlan from "./Pages/CreateLearningPlan";
+import EditLearningPlan from "./Pages/EditLearningPlan";
 function App() {
   return (
     <Router>
@@ -32,6 +36,10 @@ function App() {
         <Route path="/notification" element={<NotificationSystem />} />
         <Route path="/learning-plan" element={<LearningPlan />} />
         <Route path="/search" element={<SearchResults />} />
+        <Route path="/learning-plans" element={<LearningPlans />} />
+        <Route path="/learning-plans/:id" element={<LearningPlanDetail />} />
+        <Route path="/learning-plans/create" element={<CreateLearningPlan />} />
+        <Route path="/learning-plans/edit/:id" element={<EditLearningPlan />} />
         <Route
           path="/learning-plan-creator"
           element={<LearningPlanCreator />}
